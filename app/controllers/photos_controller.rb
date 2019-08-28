3024770499e949ad6d5642a9cec0c1656e508308
Eugeny Sobolenko <stereotype1@mail.ru>
2019-08-28 20:40:50 +0400

    add photo notification

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,6 +9,7 @@ class PhotosController < ApplicationController
     @new_photo.user = current_user
 
     if @new_photo.save
+      new_photo_notify(@event, @new_photo)
       redirect_to @event, notice: I18n.t('controllers.photos.created')
     else
       render 'events/show', alert: I18n.t('controllers.photos.error')
@@ -39,5 +40,13 @@ class PhotosController < ApplicationController
 
   def photo_params
     params.fetch(:photo, {}).permit(:photo)
+  end
+
+  def new_photo_notify(event, photo)
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [current_user.email]).uniq
+
+    all_emails.each do |mail|
+      EventMailer.photo(event, photo, mail).deliver_now
+    end
   end
 end
