38b552fb2a6beb7171eb4da484aa6f5a647bff3f
Eugeny Sobolenko <stereotype1@mail.ru>
2019-10-15 22:35:48 +0400

    add sendgrid keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,8 +101,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => Rails.application.credentials[Rails.env.to_sym][:sendgrid][:username],
+    :password       => Rails.application.credentials[Rails.env.to_sym][:sendgrid][:password],
     :domain         => 'events-liker.site',
     :enable_starttls_auto => true
   }
