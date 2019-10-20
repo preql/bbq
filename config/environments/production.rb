c419cc991e916587afa074e382d9985e36cf9e38
Eugeny Sobolenko <stereotype1@mail.ru>
2019-10-20 18:08:09 +0400

    fix active_job prefix name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
 
   config.active_job.queue_adapter = :resque
-  config.active_job.queue_name_prefix = "events-liker_#{Rails.env}"
+  config.active_job.queue_name_prefix = "bbq_#{Rails.env}"
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
